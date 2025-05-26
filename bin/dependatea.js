f9bce5e71dad91839b607a3f08179a56fd7a5bf4
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 15:08:24 +0300

    Update dependatea.js

--- bin/dependatea.js
+++ bin/dependatea.js
@@ -2,12 +2,13 @@
 const { program } = require('commander');
 const { execSync } = require('child_process');
 const { version } = require('../package.json');
+const { install, list, update } = require('../lib/commands');
 
-// TEA PATH kontrolü eklenmiş hali
+// TEA PATH kontrolü
 try {
   execSync('which tea', { stdio: 'ignore' });
 } catch {
-  console.error('Hata: TEA CLI kurulu değil. Önce şunu çalıştırın:');
+  console.error('Hata: TEA CLI kurulu değil. Kurulum için:');
   console.log('sh <(curl https://tea.xyz)');
   process.exit(1);
 }
@@ -15,6 +16,20 @@ try {
 program
   .name('dependatea')
   .version(version)
-  .description('TEA-entegreli bağımlılık yöneticisi');
+  .description('TEA-entegreli bağımlılık yöneticisi')
+  .showHelpAfterError();
 
-// ...diğer komut tanımları
+// Komutlar
+program.command('install <packages...>')
+  .description('Paket yükle')
+  .action(install);
+
+program.command('list')
+  .description('Yüklü paketleri listele')
+  .action(list);
+
+program.command('update [package]')
+  .description('Paket güncelle')
+  .action(update);
+
+program.parse();
