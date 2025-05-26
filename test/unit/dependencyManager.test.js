8648d2a4de6a6b0c2d9e030bca7789e2e7f887a5
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 04:05:02 +0300

    Update dependencyManager.test.js

--- test/unit/dependencyManager.test.js
+++ test/unit/dependencyManager.test.js
@@ -1,57 +1,35 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
-const fs = require('fs-extra');
-const execa = require('execa');
-const DependencyManager = require('../../lib/managers/dependency');
-const TeaManager = require('../../lib/tea/config');
+import { expect } from 'chai';
+import sinon from 'sinon';
+import DependencyManager from '../../src/core/DependencyManager.js';
+import fs from 'fs-extra';
 
 describe('DependencyManager', () => {
-  let depManager;
-  let teaManagerStub;
-  let execaStub;
-  let fsStub;
+  let sandbox;
+  let teaClientStub;
 
-  beforeEach(() => {
-    teaManagerStub = sinon.createStubInstance(TeaManager);
-    teaManagerStub.isEnabled = true;
-    
-    depManager = new DependencyManager(teaManagerStub);
-    
-    execaStub = sinon.stub(execa, 'command');
-    fsStub = sinon.stub(fs);
+  before(() => {
+    sandbox = sinon.createSandbox();
+    teaClientStub = {
+      connect: sandbox.stub().resolves(),
+      install: sandbox.stub().resolves({ success: true })
+    };
+    sandbox.stub(DependencyManager, 'teaClient').get(() => teaClientStub);
   });
 
   afterEach(() => {
-    sinon.restore();
+    sandbox.restore();
   });
 
   describe('#install()', () => {
-    it('should install packages with TEA', async () => {
-      execaStub.resolves({ stdout: '' });
-      fsStub.pathExists.resolves(false);
+    it('should install packages via TEA client', async () => {
+      sandbox.stub(fs, 'readJson').resolves({});
+      sandbox.stub(fs, 'writeJson').resolves();
       
-      await depManager.install(['lodash', 'express']);
+      const manager = await DependencyManager.initialize();
+      const result = await manager.install(['lodash']);
       
-      sinon.assert.calledWith(execaStub, 'tea', ['install', 'lodash', 'express']);
-    });
-  });
-
-  describe('#list()', () => {
-    it('should list all dependencies', async () => {
-      execaStub.withArgs('npm', ['list', '-g', '--depth=0', '--json'])
-        .resolves({ stdout: '{"dependencies":{"lodash":"^4.17.21"}}' });
-      
-      execaStub.withArgs('tea', ['list', '--json'])
-        .resolves({ stdout: '[{"name":"tea-pkg"}]' });
-      
-      fsStub.pathExists.resolves(true);
-      fsStub.readFile.resolves('{"dependencies":{"express":"^4.18.2"}}');
-      
-      const result = await depManager.list();
-      
-      expect(result.project.dependencies).to.have.property('express');
-      expect(result.global).to.have.property('lodash');
-      expect(result.tea).to.be.an('array');
+      expect(teaClientStub.install.calledWith('lodash')).to.be.true;
+      expect(result).to.be.an('array');
     });
   });
 });
